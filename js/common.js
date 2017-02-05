9a3319c4b0bdb97d816698adc01ff217f0964059
Ankit Aggarwal <akiwork007@gmail.com>
2017-02-05 16:11:08 +0530

    First Template After Redesigning
    New Template Applied

---
+++ js/common.js
@@ -0,0 +1,106 @@
+
+$(document).ready(function () {
+	"use strict";
+	
+	$('#header').load('header.html');
+	$('#footer').load('footer.html');
+	$('#auheader').load('../component/header.html');
+	$('#aufooter').load('../component/footer.html');
+	
+	
+
+	
+    
+	
+	//Added for markets icons
+	$("#collapse1").on("hide.bs.collapse", function () {
+		$(".marker1").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse1").on("show.bs.collapse", function () {
+		$(".marker1").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse2").on("hide.bs.collapse", function () {
+		$(".marker2").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse2").on("show.bs.collapse", function () {
+		$(".marker2").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse3").on("hide.bs.collapse", function () {
+		$(".marker3").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse3").on("show.bs.collapse", function () {
+		$(".marker3").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse4").on("hide.bs.collapse", function () {
+		$(".marker4").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse4").on("show.bs.collapse", function () {
+		$(".marker4").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse5").on("hide.bs.collapse", function () {
+		$(".marker5").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse5").on("show.bs.collapse", function () {
+		$(".marker5").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse6").on("hide.bs.collapse", function () {
+		$(".marker6").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse6").on("show.bs.collapse", function () {
+		$(".marker6").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse7").on("hide.bs.collapse", function () {
+		$(".marker7").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse7").on("show.bs.collapse", function () {
+		$(".marker7").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse8").on("hide.bs.collapse", function () {
+		$(".marker8").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse8").on("show.bs.collapse", function () {
+		$(".marker8").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse9").on("hide.bs.collapse", function () {
+		$(".marker9").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse9").on("show.bs.collapse", function () {
+		$(".marker9").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse10").on("hide.bs.collapse", function () {
+		$(".marker10").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse10").on("show.bs.collapse", function () {
+		$(".marker10").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse11").on("hide.bs.collapse", function () {
+		$(".marker11").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse11").on("show.bs.collapse", function () {
+		$(".marker11").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse12").on("hide.bs.collapse", function () {
+		$(".marker12").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse12").on("show.bs.collapse", function () {
+		$(".marker12").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+	
+	$("#collapse13").on("hide.bs.collapse", function () {
+		$(".marker13").html('<span class="glyphicon glyphicon-menu-right"></span>');
+	});
+	$("#collapse13").on("show.bs.collapse", function () {
+		$(".marker13").html('<span class="glyphicon glyphicon-menu-down"></span>');
+	});
+});
